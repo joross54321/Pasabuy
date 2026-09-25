5f496c6f648e569c4fefaa9f1a4d630ee1f4a02d
joross54321 <jorosssimo96@gmail.com>
2026-09-26 01:19:09 +0800

    refactor: remove unused google maps sdk to stabilize native builds

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import GoogleMaps
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_API_KEY_HERE")
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
